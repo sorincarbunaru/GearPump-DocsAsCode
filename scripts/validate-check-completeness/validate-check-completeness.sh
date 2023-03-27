93c035b26231972aa32cb63c3e2c3325bb51685d
Sorin Carbunaru <sorin_carbunaru@sync.ro>
2023-03-27 10:04:34 +0300

    added comments about the validation command

--- scripts/validate-check-completeness/validate-check-completeness.sh
+++ scripts/validate-check-completeness/validate-check-completeness.sh
@@ -8,10 +8,24 @@ echo "Validate and check for completeness"
 dir=$(pwd)
 echo "The current working directory is: $dir."
 
+# sh validateCheckDITA.sh -i inputFile [-c contextId] [ [-s settingsFile] | [-r reportFile]] | [--help | -help | --h | -h]
+#
+# -i inputFile    = The main input DITA map or DITA-OT project file.
+#
+# -c contextId    = The id of the context from the DITA-OT project being checked.
+#                   If not specified, the operation validates all contexts from the DITA-OT project input file.
+#                   This argument is ignored if the input file is a DITA map.
+#
+# -s settingsFile = The configuration file.
+#                   It contains the settings exported from the 'DITA Map Completeness Check' dialog box.
+#                   If not specified, the operation runs with the default settings.
+#
+# -r reportFile   = The location of the output validation and check for completeness report.
+#                   If not specified, the report is displayed in the console.
 sh bin/oxygen/scripts/validateCheckDITA.sh \
     -i $dir/dita_sources/gear-pump.ditamap \
     -s $dir/scripts/validate-check-completeness/validate-check-completeness-options.xml \
-    -r $dir/bin/tmp-vcc/vcc-result.xml
+    #-r $dir/bin/tmp-vcc/vcc-result.xml
 
 echo "-- VALIDATION RESULTS --"
 cat bin/tmp-vcc/vcc-result.xml
