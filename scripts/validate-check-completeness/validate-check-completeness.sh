70525cd9237e51889715f6d6ffbdf31f4fc618dd
Sorin Carbunaru <sorin_carbunaru@sync.ro>
2023-03-27 09:53:40 +0300

    remove sonar - WIP

--- scripts/validate-check-completeness/validate-check-completeness.sh
+++ scripts/validate-check-completeness/validate-check-completeness.sh
@@ -4,17 +4,23 @@ rm -rf bin/tmp-vcc/
 mkdir -p bin/tmp-vcc/
 
 echo "Validate and check for completeness"
+
 dir=$(pwd)
-echo "The current working directory $dir."
+echo "The current working directory is: $dir."
 
-sh bin/oxygen/scripts/validateCheckDITA.sh -i $dir/dita_sources/gear-pump.ditamap -s $dir/scripts/validate-check-completeness/validate-check-completeness-options.xml -r $dir/bin/tmp-vcc/vcc-result.xml
+sh bin/oxygen/scripts/validateCheckDITA.sh \
+    # The input DITA map
+    -i $dir/dita_sources/gear-pump.ditamap \
+    # Settings file - can be exported from Oxygen's GUI; see https://www.oxygenxml.com/doc/ug-editor/topics/dita-map-validate.html#dita-map-validate__dlentry_byl_dgk_54b
+    -s $dir/scripts/validate-check-completeness/validate-check-completeness-options.xml \
+    # The report file
+    -r $dir/bin/tmp-vcc/vcc-result.xml
 
-echo "--RESULT--"
+echo "-- VALIDATION RESULTS --"
 cat bin/tmp-vcc/vcc-result.xml
 
-echo "Convert to Sonar"
+echo "Convert to JSON"
 java -cp bin/saxon9.jar net.sf.saxon.Transform -s:bin/tmp-vcc/vcc-result.xml -xsl:scripts/validate-check-completeness/validatation-results-to-sonar.xsl > bin/tmp-vcc/vcc-result-sonar.json
 
-
-echo "--CONVERTED--"
+echo "-- VALIDATION RESULTS AS JSON --"
 cat bin/tmp-vcc/vcc-result-sonar.json
